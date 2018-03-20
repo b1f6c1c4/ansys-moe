b2b737fa2e9cee82ff7c84a64dec3521d90d5821
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-03-20 19:11:50 +0800

    Setup folder

--- src/ansysd/main.go
+++ src/ansysd/main.go
@@ -1,10 +1,27 @@
 package ansysd
 
+import (
+	"os"
+	"path/filepath"
+)
+
+var dataPath string
 var ansysPath string
+var logger func(string)
 
 // Entry setup ansysd
 // logger: write string to console or file.
-func Entry(logger func(string)) {
+func Entry(theLogger func(string)) {
+	logger = theLogger
+
+	if exeDir, err := filepath.Abs(filepath.Dir(os.Args[0])); err != nil {
+		panic(err)
+	} else {
+		dataPath = filepath.Join(exeDir, "data")
+	}
+	_ = os.MkdirAll(filepath.Join(dataPath, "raw"), os.ModePerm)
+	_ = os.MkdirAll(filepath.Join(dataPath, "temp"), os.ModePerm)
+
 	ansysPath = findAnsysExecutable()
 	logger("Ansys path: " + ansysPath)
 }
