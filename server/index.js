33bc900996b7bde0ee72748a8df7fde346471d35
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-04-03 12:07:46 +0800

    Remove old code

--- server/index.js
+++ server/index.js
@@ -1,14 +1,5 @@
-const _ = require('lodash');
 const { createServer } = require('http');
-const express = require('express');
-const cors = require('cors');
-const nocache = require('nocache');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { schema } = require('./graphql');
-const mongo = require('./mongo');
 const status = require('./status');
-const file = require('./file');
 const logger = require('./logger')('index');
 
 logger.info('Versions', process.versions);
@@ -36,69 +27,16 @@ process.on('SIGTERM', () => {
 
 const port = parseInt(process.env.PORT || '3000', 10);
 
-const app = express();
-
-app.set('trust proxy', true);
-
-app.use(cors({
-  origin: [
-    process.env.CORS_ORIGIN,
-    /^https?:\/\/localhost(:\d+)?$/,
-  ],
-  methods: ['HEAD', 'GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  maxAge: 300000,
-}));
-
-app.get('/', (req, res) => {
-  logger.trace('GET /');
+const app = (req, res) => {
   if (status) {
-    res.status(200).json(status);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(status));
   } else {
-    res.status(500).send();
+    res.statusCode = 500;
+    res.end();
   }
-});
-
-app.post(
-  '/graphql',
-  nocache(),
-  bodyParser.json(),
-  bodyParser.text({
-    type: 'application/graphql',
-  }),
-  (req, res, next) => {
-    logger.info(`${req.method} /graphql`);
-    if (req.is('application/graphql')) {
-      req.body = { query: req.body };
-    }
-    next();
-  },
-  graphqlExpress({
-    schema,
-    tracing: process.env.NODE_ENV !== 'production',
-    formatError: (err) => {
-      const e = {
-        message: err.message,
-        statusCode: _.get(err, 'originalError.statusCode'),
-        errorCode: _.get(err, 'originalError.errorCode'),
-      };
-      logger.trace('Return err', e);
-      return e;
-    },
-  }),
-);
-
-if (process.env.NODE_ENV !== 'production') {
-  app.get('/graphql', graphiqlExpress({
-    endpointURL: '/graphql',
-  }));
-}
-
-app.use('/storage', file);
-
-app.use('/', (req, res) => res.status(404).send());
+};
 
 function runApp() {
   logger.debug('http.createServer ...');
@@ -114,7 +52,6 @@ function runApp() {
 }
 
 const inits = [];
-inits.push(mongo.connect());
 
 Promise.all(inits)
   .then(runApp)
