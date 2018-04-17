dc344190347c21094358512911b0dd43907aa004
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-04-17 15:04:23 +0800

    Fix bug

--- server/etcd.js
+++ server/etcd.js
@@ -2,16 +2,14 @@ const db = {};
 
 module.exports = {
   connect: () => {},
-  etcd: {
-    get: (key) => ({
-      number: async () => db[key] && parseInt(db[key], 10),
-      json: async () => db[key] && JSON.parse(db[key]),
+  get: (key) => ({
+    number: async () => db[key] && parseInt(db[key], 10),
+    json: async () => db[key] && JSON.parse(db[key]),
+  }),
+  put: (key) => ({
+    value: (value) => ({
+      exec: async () => { db[key] = String(value); },
     }),
-    put: (key) => ({
-      value: (value) => ({
-        exec: async () => { db[key] = String(value); },
-      }),
-    }),
-  },
+  }),
   mock: () => db,
 };
