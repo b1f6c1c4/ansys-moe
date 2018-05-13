9eeda3b228b65fd56083598619a0348533a264e3
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-05-13 14:54:41 +0800

    Remove unused

--- app/translations/index.js
+++
@@ -1,2 +0,0 @@
-module.exports.en = require('./en.json');
-module.exports.zh = require('./zh.json');
--- internals/webpack/i18n.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash');
-
-module.exports = (res) => _.mapValues(
-  res,
-  (lst) => _.chain({})
-    .assign(res.en)
-    .assign(lst)
-    .value(),
-);
